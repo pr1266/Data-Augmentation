3a73cb8d338cde7b1fd836830929ab6bb9b8fa8c
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-07-21 00:13:01 +0430

    ''

--- bounding_box_augmentation.py
+++ bounding_box_augmentation.py
@@ -1,19 +1,3 @@
-class Sequence(object):
-
-    def __init__(self, augmentations, probs = 1):
-        #! self.augmentation stores the list of augmentation that we want to use
-        #! self.probs holds the probablity of each augmentation to be applied
-        self.augmentations = augmentations
-        self.probs = probs
-        
-    def call(self, image, bbox):
-        for i, augmentation in enumerate(self.augmentations):
-            #! if type of self.prob is a list, each element of it shows the probablity of its corresponding element in augmentation array
-            #! but if its a number, it applies for all elements of self.augmentation array
-            if type(self.probs) == list:
-                prob = self.prob[i]
-            else:
-                prob = self.probs
-            
-            img, bbox = augmentation(img, bbox)
-        return img, bbox
+from cv2 import cv2
+from matplotlib import pyplot as plt
+import albumentations as A
